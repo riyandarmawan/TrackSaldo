7082ce075cadcdf30484bb5612195cee8186b3f7
riyandarmawan042 <riyandarmawan042@gmail.com>
2025-06-24 10:02:15 +0000

    add: layout for dashboard (unfinished)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,35 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardAction, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { TrendingDown, TrendingUp } from "lucide-react";
 
 export default function Dashboard() {
     return (
-      <div className="container py-8 grid grid-cols-2 gap-4">
-        <Card className="@container/card">
-            <CardHeader>
-                <CardDescription>Income</CardDescription>
-                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">Rp100.000</CardTitle>
-            </CardHeader>
-        </Card>
+        <div className="container py-8 grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>Income</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">Rp100.000</CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <TrendingUp />
+                            +12.5%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
+            </Card>
 
-        <Card className="@container/card">
-            <CardHeader>
-                <CardDescription>Outcome</CardDescription>
-                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">Rp50.000</CardTitle>
-            </CardHeader>
-        </Card>
-      </div>
+            <Card className="@container/card">
+                <CardHeader>
+                    <CardDescription>Outcome</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">Rp50.000</CardTitle>
+                    <CardAction>
+                        <Badge variant="outline">
+                            <TrendingDown />
+                            -8.5%
+                        </Badge>
+                    </CardAction>
+                </CardHeader>
+            </Card>
+        </div>
     );
-  }
-  
+}
