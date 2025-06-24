40f0066e2da41723679e5993f00673fc715fcd91
riyandarmawan042 <riyandarmawan042@gmail.com>
2025-06-24 09:37:09 +0000

    add: income and outcome component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,22 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Dashboard() {
     return (
-      <div></div>
+      <div className="container py-8 grid grid-cols-2 gap-4">
+        <Card className="@container/card">
+            <CardHeader>
+                <CardDescription>Income</CardDescription>
+                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">Rp100.000</CardTitle>
+            </CardHeader>
+        </Card>
+
+        <Card className="@container/card">
+            <CardHeader>
+                <CardDescription>Outcome</CardDescription>
+                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">Rp50.000</CardTitle>
+            </CardHeader>
+        </Card>
+      </div>
     );
   }
   
